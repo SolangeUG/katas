b552367d192177d136414c819e8f2afcf98f5738
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-11-03 23:15:06 +0000

    Implement main app entry point

--- leap-year/src/main/java/com/codurance/App.java
+++ leap-year/src/main/java/com/codurance/App.java
@@ -1,13 +1,20 @@
 package com.codurance;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main( String[] args ) {
+        int[] years = {1997, 1996, 1600, 1800};
+
+        for (int value : years) {
+            LeapYear year = new LeapYear(value);
+
+            Boolean result = year.isLeapYear();
+            String output = "a leap year";
+
+            System.out.println(
+                    String.format("%d is %s",
+                            value,
+                            result ? output : "not " + output));
+        }
     }
 }
